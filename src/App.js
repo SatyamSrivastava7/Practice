c179ac3e68f2e7345c0400950d6725895dbc9a34
Satyam Srivastava <satyam.srivastava@maersk.com>
2022-01-25 09:24:48 +0530

    Code Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Link to= "/">AllMeetups</Link>
+      <Link to= "/new-meetup">New Meet Up</Link>
+      <Link to= "/favorites">Favorites</Link>
+
     </div>
   );
 }
 
 export default App;
+// function Users() {
+//   /* All <Route path> and <Link to> values in this
+//      component will automatically be "mounted" at the
+//      /users URL prefix since the <Users> element is only
+//      ever rendered when the URL matches /users/*
+//   */
+//   return (
+//     <div>
+//       <nav>
+//         <Link to="me">My Profile</Link>
+//       </nav>
+
+//       <Routes>
+//         <Route path="/" element={<UsersIndex />} />
+//         <Route path=":id" element={<UserProfile />} />
+//         <Route path="me" element={<OwnUserProfile />} />
+//       </Routes>
+//     </div>
+//   );
+// }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import App from "./App";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+//import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <BrowserRouter>
+  <Layout>
+    <Routes>
+      <Route exact path="/" element={<AllMeetupsPage />} />
+      <Route exact path="/new-meetup" element={<NewMeetupPage />} />
+      <Route exact path="/favorites" element={<FavoritesPage />} />
+    </Routes>
+    </Layout>
+  </BrowserRouter>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById("root")
+);
